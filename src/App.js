69a2ff7ddb76e9d8855e0052a9fb75cbcda64076
Andrei Constantinescu <andreiconstantinescumail@gmail.com>
2018-03-28 18:38:22 +0300

    Added functionality to searchbox and styled the app.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "./CardList";
 import {robots} from "./robots";
 import SearchBox from "./SearchBox";
+import './App.css';
 
 
 class App extends Component {
@@ -11,23 +12,23 @@ class App extends Component {
       robots: robots,
       searchfield: ''
     }
-    this.onSearchChange = this.onSearchChange.bind(this);
-   }searchChange
+   }
 
   onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+
+  }
+
+  render() {
     const filteredRobots =
       this.state.robots.filter(robots => {
         return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
       })
-      console.log(filteredRobots);
-  }
-
-  render() {
     return (
       <div className="tc">
-        <h1> RoboFriends</h1>
+        <h1 className="f1"> RoboFriends</h1>
         <SearchBox searchChange = {this.onSearchChange}/>
-        <CardList robots = {this.state.robots} />
+        <CardList robots = {filteredRobots} />
       </div>
     );
   }
