b52ebc63c3673ffe588f694b82c1363900e8e3c4
Andrei Constantinescu <andreiconstantinescumail@gmail.com>
2018-03-28 20:45:27 +0300

    Added loading bar and now fetching data from JSON API.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
-import {robots} from "./robots";
 import SearchBox from "./SearchBox";
 import './App.css';
 
@@ -9,14 +8,19 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
    }
 
+   componentDidMount() {
+     fetch('https://jsonplaceholder.typicode.com/users')
+     .then(res => res.json())
+     .then(users => this.setState({robots: users}));
+   }
+
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value })
-
   }
 
   render() {
@@ -24,13 +28,17 @@ class App extends Component {
       this.state.robots.filter(robots => {
         return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
       })
-    return (
-      <div className="tc">
-        <h1 className="f1"> RoboFriends</h1>
-        <SearchBox searchChange = {this.onSearchChange}/>
-        <CardList robots = {filteredRobots} />
-      </div>
-    );
+      if(this.state.robots.length === 0) {
+        return <h1 className="tc">Loading...</h1>
+      } else {
+        return (
+          <div className="tc">
+            <h1 className="f1">RoboFriends</h1>
+            <SearchBox searchChange = {this.onSearchChange}/>
+            <CardList robots = {filteredRobots} />
+          </div>
+        );
+      }
   }
 
 }
